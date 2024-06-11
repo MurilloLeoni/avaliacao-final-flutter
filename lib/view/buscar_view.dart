a0d65cf3b398e37c589bda1e38b84b2f2a4d43a4
CGermanno <carlosg.gasparello@gmail.com>
2024-06-11 15:36:27 -0300

    att

--- lib/view/buscar_view.dart
+++ lib/view/buscar_view.dart
@@ -1,9 +1,14 @@
-//teste
+
+
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../controller/tarefa_controller.dart';
+import '../controller/login_controller.dart';
+import '../model/tarefa.dart';
+import 'alterar_dados_view.dart'; // Importar a nova tela
 
 class BuscaView extends StatefulWidget {
   @override
@@ -13,6 +18,9 @@ class BuscaView extends StatefulWidget {
 class _BuscaViewState extends State<BuscaView> {
   var txtSearch = TextEditingController();
   String searchQuery = "";
+
+  var txtTitulo = TextEditingController();
+  var txtDescricao = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -82,14 +90,27 @@ class _BuscaViewState extends State<BuscaView> {
                           return ListTile(
                             title: Text(item['titulo']),
                             subtitle: Text(item['descricao']),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => DetalhesTarefaView(id: id, item: item),
-                                ),
-                              );
-                            },
+                            trailing: SizedBox(
+                              width: 80,
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.edit_outlined),
+                                    onPressed: () {
+                                      txtTitulo.text = item['titulo'];
+                                      txtDescricao.text = item['descricao'];
+                                      salvarTarefa(context, docId: id);
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.delete_outlined),
+                                    onPressed: () {
+                                      TarefaController().excluir(context, id);
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
                           );
                         },
                       );
@@ -102,39 +123,72 @@ class _BuscaViewState extends State<BuscaView> {
       ),
     );
   }
-}
 
-class DetalhesTarefaView extends StatelessWidget {
-  final String id;
-  final Map item;
-
-  DetalhesTarefaView({required this.id, required this.item});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(item['titulo']),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Título:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+  void salvarTarefa(context, {docId}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text((docId == null) ? "Adicionar Tarefa" : "Editar Tarefa"),
+          content: SizedBox(
+            height: 250,
+            width: 300,
+            child: Column(
+              children: [
+                TextField(
+                  controller: txtTitulo,
+                  decoration: InputDecoration(
+                    labelText: 'Título',
+                    prefixIcon: Icon(Icons.description),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 15),
+                TextField(
+                  controller: txtDescricao,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    labelText: 'Descrição',
+                    alignLabelWithHint: true,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
             ),
-            Text(item['titulo']),
-            SizedBox(height: 20),
-            Text(
-              'Descrição:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          actionsPadding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+          actions: [
+            TextButton(
+              child: Text("fechar"),
+              onPressed: () {
+                txtTitulo.clear();
+                txtDescricao.clear();
+                Navigator.of(context).pop();
+              },
             ),
-            Text(item['descricao']),
+            ElevatedButton(
+              child: Text("salvar"),
+              onPressed: () {
+                var t = Tarefa(
+                  LoginController().idUsuario(),
+                  txtTitulo.text,
+                  txtDescricao.text,
+                );
+
+                if (docId == null) {
+                  TarefaController().adicionar(context, t);
+                } else {
+                  TarefaController().atualizar(context, docId, t);
+                }
+
+                txtTitulo.clear();
+                txtDescricao.clear();
+                Navigator.of(context).pop(); // Fechar o diálogo ao salvar
+              },
+            ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
