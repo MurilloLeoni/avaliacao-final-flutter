6fa5c3af40d01ca2a403907008685ff4093214c5
CGermanno <carlosg.gasparello@gmail.com>
2024-06-11 15:35:58 -0300

    buscaatt

--- lib/view/buscar_view.dart
+++ lib/view/buscar_view.dart
@@ -1,8 +1,6 @@
 //teste
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../controller/tarefa_controller.dart';
